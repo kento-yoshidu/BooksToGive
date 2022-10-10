0d1458556adc81abb4dc830d1fa307f87c0557a2
kento <kento@toriwatari.bird>
2022-10-10 12:44:49 +0900

    ✨ #6 テーブル定義の変更

--- prisma/migrations/20221010034354_update/migration.sql
+++ prisma/migrations/20221010034354_update/migration.sql
@@ -2,6 +2,8 @@
 CREATE TABLE "books" (
     "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
+    "isbn" TEXT NOT NULL,
+    "rating" INTEGER NOT NULL,
 
     CONSTRAINT "books_pkey" PRIMARY KEY ("id")
 );
