fedbf9b40fa785ada5d90c9b9a0e93460a347965
kento <kento@toriwatari.bird>
2022-10-10 13:53:35 +0900

    ✨ 本一覧を表示

--- data/books.ts
+++ data/books.ts
@@ -3,5 +3,25 @@ export const books= [
     title: "HTML5/CSS3モダンコーディング フロントエンドエンジニアが教える3つの本格レイアウト スタンダード・グリッド・シングルページレイアウトの作り方",
     isbn: "9784798141572",
     rating: 3
+  },
+  {
+    title: "1冊ですべて身につくHTML & CSSとWebデザイン入門講座",
+    isbn: "9784797398892",
+    rating: 4
+  },
+  {
+    title: "新しいLinuxの教科書",
+    isbn: "9784797380941",
+    rating: 5
+  },
+  {
+    title: "図解まるわかり ネットワークのしくみ",
+    isbn: "9784798157498",
+    rating: 4
+  },
+  {
+    title: "React.js&Next.js超入門 第2版",
+    isbn: "9784798063980",
+    rating: 1
   }
 ]
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,28 @@
-import type { NextPage } from 'next'
+import prisma from "../lib/prisma"
 
-const Home: NextPage = () => {
-  return (
-    <h1>Next.js</h1>
+import { GetStaticProps } from "next"
+import { Book } from "../types/Book"
+
+const Home = ({ books }: { books: Book[] }) => (
+    <>
+      <h1>いらない本リスト</h1>
+
+      {books.map((book) => (
+        <div key={`${book.id}`}>
+          <h2>{book.title}</h2>
+          <p>{book.isbn}</p>
+          <p>{book.rating}</p>
+        </div>
+      ))}
+    </>
   )
-}
 
 export default Home
+
+export const getStaticProps: GetStaticProps = async () => {
+  const books = await prisma.book.findMany()
+
+  return  {
+    props: { books }
+  }
+}
