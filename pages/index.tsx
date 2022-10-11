e9b81bb343e259d6a52666e5a4db504a1b2cad83
kento <kento@toriwatari.bird>
2022-10-11 20:21:49 +0900

    ✨ #12 apiからデータ取得OK

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,23 @@ import prisma from "../lib/prisma"
 import { GetStaticProps } from "next"
 import { Book } from "../types/Book"
 
-const Home = ({ books }: { books: Book[] }) => (
+import useSWR from "swr"
+
+const fetcher = (...args) => fetch(...args).then(res => res.json())
+
+const Home = ({ books }: { books: Book[] }) => {
+  const { data, error } = useSWR("https://www.googleapis.com/books/v1/volumes?q=夏目漱石", fetcher)
+
+  if (error) return <div>An error has occurred.</div>
+  if (!data) return <div>Loading...</div>
+
+  return (
     <>
       <h1>いらない本リスト</h1>
+
+      <img
+        src={data.items[0].volumeInfo.imageLinks.thumbnail}
+      />
 
       {books.map((book) => (
         <div key={`${book.id}`}>
@@ -16,6 +30,7 @@ const Home = ({ books }: { books: Book[] }) => (
       ))}
     </>
   )
+}
 
 export default Home
 
