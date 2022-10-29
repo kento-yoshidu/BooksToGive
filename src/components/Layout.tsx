3e6b456162547d9b569b6bcf1727c2e66c1f3674
kento <kento@toriwatari.bird>
2022-10-29 19:49:35 +0900

    ✨ Create Layout.tsx

---
+++ src/components/Layout.tsx
@@ -0,0 +1,36 @@
+import React from "react"
+import Link from "next/link"
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config } from "@fortawesome/fontawesome-svg-core"
+config.autoAddCss = false
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <div className="w-5/6 md:w-1/2 mx-auto mt-10 min-h-[75vh]">
+        <header className="mb-6 md:mb-8">
+          <h1 className="text-4xl">BooksToGive</h1>
+        </header>
+
+        {children}
+      </div>
+
+      <footer className="mt-20 py-4 bg-gray-900 text-center">
+        <Link href="https://github.com/kento-yoshidu/BooksToGive">
+          <a className='cursor'>
+            <FontAwesomeIcon
+              className="text-4xl text-white"
+              icon={faGithub}
+            />
+          </a>
+        </Link>
+      </footer>
+    </>
+  )
+}
+
+export default Layout
