fe7c974885739d0ca1a012035e1bbe59ce6325e0
kento <kento@toriwatari.bird>
2022-10-30 12:14:56 +0900

    ✨ #48 Create AuthWrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import { SessionProvider } from "next-auth/react"
+import AuthWrapper from "../src/components/AuthWrapper"
 import type { AppProps } from 'next/app'
 import "../src/styles/global.scss"
 
 function MyApp({ Component, pageProps: { session, ...pageProps} }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <AuthWrapper>
+        <Component {...pageProps} />
+      </AuthWrapper>
     </SessionProvider>
   )
 }
