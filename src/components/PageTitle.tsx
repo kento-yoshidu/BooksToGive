155e3fe8a293b4750b8acec554d8393dd1e00648
kento <kento@toriwatari.bird>
2022-11-01 20:34:08 +0900

    ✨ #48 フォームのCSS更新

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,6 +1,6 @@
 const PageTitle = ({ pageTitle }: { pageTitle: string }) => {
   return (
-    <h2 className="text-2xl mb-8">■ {pageTitle}</h2>
+    <h2 className="text-2xl mb-4">■ {pageTitle}</h2>
   )
 }
 export default PageTitle
