71c6119a85df091c92e83b312b1fce6f7845c831
kento <kento@toriwatari.bard>
2023-09-14 20:33:43 +0900

    ✨ ソートのローカルstateを削除

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,19 +1,13 @@
 import { create } from 'zustand'
 
 type State = {
-  count: number
-  sortState: boolean
-  changeSort: () => void
+  isSorted: boolean
+  changeSortState: () => void
 }
 
 const useStore = create<State>((set) => ({
-  count: 1,
-  sortState: false,
-  changeSort: () => set((state) => ({ sortState: !state.sortState }))
-  /*
-  decrease: () => set((state) => ({ count: state.count - 1 })),
-  reset: () => set({ count: 0 }),
-  */
+  isSorted: false,
+  changeSortState: () => set((state) => ({ isSorted: !state.isSorted }))
 }))
 
 export default useStore
